c2e47c8acb0b1c1f23a2fb4643396cafc2c2364a
neutral-jungle-creep <arrrseny11@gmail.com>
2023-02-05 00:24:33 +0300

    edit nats-streaming.go

--- internal/delivery/nats/nats-streaming.go
+++ internal/delivery/nats/nats-streaming.go
@@ -1,11 +1,9 @@
-package delivery
+package nats
 
 import (
-	"encoding/json"
 	"github.com/nats-io/nats.go"
 	"github.com/nats-io/stan.go"
 	"nats-listener/configs"
-	"nats-listener/internal/domain"
 )
 
 type NatsQueue struct {
@@ -15,34 +13,31 @@ type NatsQueue struct {
 
 func NewNatsQueue(config *configs.NatsConfig) (*NatsQueue, error) {
 	conn, err := nats.Connect(config.URL)
-	// TODO обработать исключение
+	if err != nil {
+		return nil, err
+	}
+
 	sc, err := stan.Connect(config.ClusterID, config.ClientID, stan.NatsConn(conn))
 	if err != nil {
 		return nil, err
 	}
+
 	return &NatsQueue{
 		conn: sc,
 		subj: config.Subj,
 	}, nil
 }
 
-// TODO переименовать
 type OrdersMessageQueue interface {
-	OnNewMessage(listener func(option domain.Order))
+	OnNewMessage(listener func(order string)) (e error)
 	Close()
 }
 
-func (nq *NatsQueue) OnNewMessage(listener func(o domain.Order)) {
+func (nq *NatsQueue) OnNewMessage(handler func(order string)) (e error) {
 	nq.conn.Subscribe(nq.subj, func(msg *stan.Msg) {
-		var order = domain.Order{}
-
-		err := json.Unmarshal(msg.Data, &order)
-		if err != nil {
-			//TODO
-		}
-		//TODO validation
-		listener(order)
+		handler(string(msg.Data))
 	})
+	return nil
 }
 
 func (nq *NatsQueue) Close() {
