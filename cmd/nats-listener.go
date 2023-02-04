2e51d87b132695eb1b8c5fa6eb98b76f0138310c
neutral-jungle-creep <arrrseny11@gmail.com>
2023-02-05 01:14:50 +0300

    edit main

--- cmd/nats-listener.go
+++ cmd/nats-listener.go
@@ -1,30 +1,64 @@
 package main
 
 import (
-	"flag"
-	"github.com/sirupsen/logrus"
-	"github.com/spf13/viper"
+	"context"
+	"nats-listener/configs"
+	"nats-listener/internal/caching"
 	"nats-listener/internal/delivery"
+	pkg "nats-listener/internal/delivery/http"
+	"nats-listener/internal/delivery/nats"
+	"nats-listener/internal/service"
+	"nats-listener/internal/storage"
+	"nats-listener/internal/storage/pgSQL"
+	"nats-listener/pkg/logger"
+	"net/http"
 )
 
 func main() {
-	if err := initConfig(); err != nil {
-		logrus.Fatalf("error init configuration, %s", err.Error())
+	log := logger.NewLogger()
+	log.Info("logger has been started")
+
+	config := configs.NewConfig()
+	log.Info("config has been init")
+
+	dbConn, err := pgSQL.NewPgConnect(config.DataBase)
+	if err != nil {
+		log.Fatalf("can't connect to data base by link: %s", err.Error())
 	}
+	log.Infof("connect to data base is successful by link: %s", config.DataBase.ConnLink)
+	defer dbConn.Close(context.Background())
 
-	delivery.Run()
-}
+	//todo переделать позже
+	cache := caching.NewCache()
+	if err := cache.FillCache(dbConn); err != nil {
+		log.Errorf("can't fill cache: %s", err.Error())
+	}
+	log.Info("cache filled successfully")
 
-func initConfig() error {
-	var configPath, configFile string
+	stor := storage.NewStorage(dbConn, cache, log)
+	serv := service.NewService(stor)
+	handler := delivery.NewHandler(serv)
 
-	flag.StringVar(&configPath, "path", "configs", "Path to config file")
-	flag.StringVar(&configFile, "config", "config", "Name of config file")
-	flag.StringVar(&configPath, "p", "configs", "Path to config file")
-	flag.StringVar(&configFile, "c", "config", "Name of config file")
-	flag.Parse()
+	natsQueue, err := nats.NewNatsQueue(config.Nats)
+	if err != nil {
+		log.Fatalf("can't connect to nats: %s", err.Error())
+	}
+	log.Infof("connect to nats is successfully: url = [%s], cluster = [%s], client = [%s]",
+		config.Nats.URL, config.Nats.ClusterID, config.Nats.ClientID)
+	var messageQueue nats.OrdersMessageQueue = natsQueue
+	defer messageQueue.Close()
 
-	viper.AddConfigPath(configPath)
-	viper.SetConfigName(configFile)
-	return viper.ReadInConfig()
+	messageQueue.OnNewMessage(handler.HandleNewOrder)
+
+	go func() {
+		server := new(pkg.Server)
+
+		log.Infof("server started on: http://localhost:%s", config.Http.Port)
+
+		http.HandleFunc("/get-order", handler.GetOrderById)
+		log.Fatal(server.Run(config.Http.Port, nil))
+	}()
+
+	ch := make(chan int)
+	<-ch
 }
